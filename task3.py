7862f2cf6fd5507ddda4c627854fc031b8db86cd
Sebkd <sebkd@mail.ru>
2021-01-18 20:01:14 +0700

    coding task3

--- task3.py
+++ task3.py
@@ -15,17 +15,17 @@ class Worker:
         self.position = position
         self._income = income
 
-    def get_income(self):
-        return self._income()
-
 class Position(Worker):
+    def __init__(self, name, surname, position, income):
+        super(Position, self).__init__(name, surname, position, income)
 
     def get_full_name(self):
         return str(self.surname + ' ' + self.name)
 
     def get_total_income(self):
-        return Worker.get_income(self)
+        return (self._income.get('wage') + self._income.get('bonus'))
 
-docker = Position('Andre', 'Grey', 'docker', {'wage': 2000, 'bonus': 500})
-print(docker.get_full_name())
-print(docker.get_total_income())
+docker = Position(f'{input("Введите имя: ")}', f'{input("Введите фамилию: ")}', f'{input("Введите должность: ")}',
+                  {'wage': f'{input("Введите оклад: ")}', 'bonus' : f'{input("Введите премию: ")}'})
+print(f'Full name worker - {docker.get_full_name()}')
+print(f'His total income: {docker.get_total_income()}')
